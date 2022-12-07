c64d96fbc4b088b6fb3fa2a6d600189ee5ce56cc
1unaram <haram620@icloud.com>
2022-12-07 14:36:44 +0900

    feat: intro 변경

--- src/oop_project4_dodo/IntroPanel.java
+++ src/oop_project4_dodo/IntroPanel.java
@@ -2,9 +2,11 @@ package oop_project4_dodo;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Image;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
@@ -35,7 +37,7 @@ public class IntroPanel extends JPanel {
 
 		dodo = new ObjBlock("img/dodo_right.png", BlockArr, null, "Big");
 		_is = new WordBlock("img/_is.png", BlockArr, null, "Big");
-		_win = new WordBlock("img/_win.png", BlockArr, null, "Big");
+		_win = new WordBlock("img/_you.png", BlockArr, null, "Big");
 
 		dodo.setBounds(330, 180, 120, 120);
 		_is.setBounds(570, 300, 120, 120);
@@ -50,6 +52,16 @@ public class IntroPanel extends JPanel {
 	public void playIntro() {
 		Timer t1 = new Timer();
 
+		ImageIcon icon_down = new ImageIcon("img/dodo_down.png");
+		Image img_down = icon_down.getImage();
+		Image changeImg_down = img_down.getScaledInstance(120, 120, Image.SCALE_SMOOTH);
+		ImageIcon changeIcon_down = new ImageIcon(changeImg_down);
+		
+		ImageIcon icon_right = new ImageIcon("img/dodo_right.png");
+		Image img_right = icon_right.getImage();
+		Image changeImg_right = img_right.getScaledInstance(120, 120, Image.SCALE_SMOOTH);
+		ImageIcon changeIcon_right = new ImageIcon(changeImg_right);
+
 		TimerTask task1 = new TimerTask() {
 
 			int i = 0, j = 0, cnt = 0;
@@ -59,9 +71,11 @@ public class IntroPanel extends JPanel {
 
 				if (cnt < 7) {
 					if (cnt < 3) {
+						dodo.setIcon(changeIcon_down);
 						dodo.setLocation(270, 180 + 60 * i);
 						i++;
 					} else {
+						dodo.setIcon(changeIcon_right);
 						dodo.setLocation(270 + 60 * j, 300);
 						j++;
 					}
